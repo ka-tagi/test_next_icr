f3242c03af35d05049b93b43a00c451ef6d0e7d2
d2cdot_kimura <kaori.kimura@d2cdot.co.jp>
2022-05-27 11:59:14 +0900

    preview 処理リファクタリング

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,3 +1,5 @@
+import { getArticle } from '@/lib/api.js';
+
 export const API_URL = process.env.API_URL;
 export const API_KEY = process.env.API_KEY;
 
@@ -10,19 +12,25 @@ export const API_KEY = process.env.API_KEY;
 
   const url = `${API_URL}blogs/${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`;
 
-  const previewRes = await fetch(url,
-    { headers: { 'X-MICROCMS-API-KEY': API_KEY } }
-  );
-  const content = await previewRes.json();
+  const params = {
+    fields: id,
+    draftKey: req.query.draftKey,
+  }
+
+  const content = await getArticle(req.query.slug, params);
 
   if (!content) {
     return res.status(401).json({ message: 'Invalid slug' });
   }
 
-  res.setPreviewData({
-    slug: content.id,
-    draftKey: req.query.draftKey,
-  });
+  res.setPreviewData(
+    {
+      draftKey: req.query.draftKey,
+    },
+    {
+      maxAge: 60 * 30, // プレビューモードのCookieの期限を30分にする
+    },
+  );
   res.writeHead(307, { Location: `/posts/${content.id}` });
   res.end('Preview mode enabled');
 };
