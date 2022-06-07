26dc9909ed72bd8959ddfc04525168054c6f345c
d2cdot_kimura <kaori.kimura@d2cdot.co.jp>
2022-06-07 17:51:32 +0900

    ページネーションに前後のリンク追加

--- src/components/oganisms/pagination/index.js
+++ src/components/oganisms/pagination/index.js
@@ -6,24 +6,41 @@ import styles from './styles.module.scss';
 // template --------------------------------------------
 const Pagination = ({ pageList }) => {
   const paginationData = usePagenation(pageList);
+  const lastPage = paginationData.pages[paginationData.pages.length - 1];
 
   return (
-    <ul className={styles.pagination}>
-    {paginationData.pages.map((page) => (
-      <li key={page}>
-        [
-        {paginationData.isCurrent === page
-          ?
-          <span className={styles.isCurrent}>{paginationData.isCurrent}</span>
-          :
-          <Link href={`/posts/pages/${page}`}>
-            <a>{page}</a>
-          </Link>
-        }
-        ]
-      </li>
-    ))}
-    </ul>
+    <nav className={styles.pagination}>
+      <div className={styles.first}>
+        <Link href='/posts/pages/1'><a>&lt;&lt;</a></Link>
+      </div>
+      <div className={ paginationData.isCurrent -1 > 0 ? styles.prev : styles.prev0 }>
+        <Link href={`/posts/pages/${paginationData.isCurrent - 1}`}><a>&lt;</a></Link>
+      </div>
+
+      <ul className={styles.paginationUl}>
+      {paginationData.pages.map((page) => (
+        <li key={page}>
+          [
+          {paginationData.isCurrent === page
+            ?
+            <span className={styles.isCurrent}>{paginationData.isCurrent}</span>
+            :
+            <Link href={`/posts/pages/${page}`}>
+              <a>{page}</a>
+            </Link>
+          }
+          ]
+        </li>
+      ))}
+      </ul>
+
+      <div className={paginationData.isCurrent < lastPage ? styles.next : styles.next0}>
+        <Link href={`/posts/pages/${paginationData.isCurrent + 1}`}><a>&gt;</a></Link>
+      </div>
+      <div className={styles.last}>
+        <Link href={`/posts/pages/${lastPage}`}><a>&gt;&gt;</a></Link>
+      </div>
+    </nav>
   );
 }
 
